e94e2d7d674b488c5091b7b0d50ace12cb45661f
Tu Nombre <tu.email@ejemplo.com>
2025-02-02 23:09:07 -0300

    Añadir keystore para firma de APK

---
+++ resize_icons.py
@@ -0,0 +1,16 @@
+﻿from PIL import Image
+import os
+
+logo_path = 'frontend/public/logo.png'
+sizes = [
+    (48, 'frontend/android/app/src/main/res/mipmap-mdpi/ic_launcher.png'),
+    (72, 'frontend/android/app/src/main/res/mipmap-hdpi/ic_launcher.png'),
+    (96, 'frontend/android/app/src/main/res/mipmap-xhdpi/ic_launcher.png'),
+    (144, 'frontend/android/app/src/main/res/mipmap-xxhdpi/ic_launcher.png'),
+    (192, 'frontend/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png')
+]
+
+logo = Image.open(logo_path)
+for size, output_path in sizes:
+    resized_logo = logo.resize((size, size), Image.LANCZOS)
+    resized_logo.save(output_path)
